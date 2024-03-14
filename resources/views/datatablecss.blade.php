a64868ff25ebb8bf3cf9b85a7e6d4dcb074a87b5
Konarch Patterns247 <konarch@patterns247.net>
2024-03-15 02:10:15 +0530

    changes

---
+++ resources/views/datatablecss.blade.php
@@ -0,0 +1,12 @@
+
+
+<link href="https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css" rel=" stylesheet">
+<!--Replace with your tailwind.css once created-->
+
+
+<!--Regular Datatables CSS-->
+<link href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css" rel="stylesheet">
+<!--Responsive Extension Datatables CSS-->
+<link href="https://cdn.datatables.net/responsive/2.2.3/css/responsive.dataTables.min.css" rel="stylesheet">
+
+<link href="{{asset('css/datatable.css')}}" rel="stylesheet">
