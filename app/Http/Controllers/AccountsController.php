85e56cc6f53c8931532d26e139d81c3088f8b149
Konarch Patterns247 <konarch@patterns247.net>
2024-04-02 02:51:13 +0530

    migrations

---
+++ app/Http/Controllers/AccountsController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Company;
+use App\Models\CompanyLocation;
+
+use App\Models\Country;
+use App\Models\State;
+use App\Models\City;
+use App\Models\CompanyPhone;
+use App\Models\CompanyEmail;
+use App\Models\CompanyBusiness;
+
+class AccountsController extends Controller
+{
+    public function create(){
+        return view('account.create');
+    }
+    public function index(){
+        return view('account.index');
+    }
+    public function update(Request $request){
+        $id=$request->id;
+        $companyloc=CompanyLocation::select("*")->where('company_id',$id)->get();
+        if(count($companyloc)>0)
+        {
+        $countryidss=$companyloc[0]['country_id'];
+        $stateidss=$companyloc[0]['state_id'];
+        $cityidss=$companyloc[0]['city_id'];
+        }
+        else{
+            $countryidss=249;
+            $stateidss=4122;
+            $cityidss=48357;
+        }
+        return view('account.update',compact('id','countryidss','stateidss','cityidss'));
+    }
+}
---
+++ app/Http/Controllers/LocationController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Country;
+use App\Models\State;
+use App\Models\City;
+
+class LocationController extends Controller
+{
+        /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function index()
+    {
+        $data['countries'] = Country::get(["name", "id"]);
+        return view('dropdown', $data);
+    }
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function fetchState(Request $request)
+    {
+        $data['states'] = State::where("country_id", $request->country_id)
+                                ->get(["name", "id"]);
+
+        return response()->json($data);
+    }
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function fetchCity(Request $request)
+    {
+        $data['cities'] = City::where("state_id", $request->state_id)
+                                    ->get(["name", "id"]);
+
+        return response()->json($data);
+    }
+}
