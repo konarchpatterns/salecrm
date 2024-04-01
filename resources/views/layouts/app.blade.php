e459155508d441638decc9b327f84e3f7607f0b9
Konarch Patterns247 <konarch@patterns247.net>
2024-04-02 01:37:44 +0530

    migrations

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,16 +42,23 @@
                 <div class="px-3 py-3 lg:px-5 lg:pl-3">
                   <div class="flex items-center justify-between">
                     <div class="flex items-center justify-start rtl:justify-end">
-                      <button data-drawer-target="logo-sidebar" data-drawer-toggle="logo-sidebar" aria-controls="logo-sidebar" type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+                      <button data-drawer-target="logo-sidebar" data-drawer-toggle="logo-sidebar" aria-controls="logo-sidebar" type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
                           <span class="sr-only">Open sidebar</span>
                           <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                              <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
                           </svg>
                        </button>
-                      <a href="https://flowbite.com" class="flex ms-2 md:me-24">
-                        <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 me-3" alt="FlowBite Logo" />
-                        <span class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">Flowbite</span>
+                      <a href="https://flowbite.com" class="flex  ml-2">
+                        <img src={{asset('logo.png')}} class="h-10  me-2" alt="FlowBite Logo" />
+                        {{-- <span class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">Flowbite</span> --}}
                       </a>
+                      <div class = "flex items-center justify-end ltl:justify-end   sm:block ">
+
+                        <svg xmlns="http://www.w3.org/2000/svg" class= "ml-12  " width="24" height="24"   id= "btn11" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-ellipsis-vertical "><circle cx="12" cy="12" r="1"/><circle cx="12" cy="5" r="1"/><circle cx="12" cy="19" r="1"/></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" class= "ml-12  " width="24" height="24"   id= "btn12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-ellipsis-vertical "><circle cx="12" cy="12" r="1"/><circle cx="12" cy="5" r="1"/><circle cx="12" cy="19" r="1"/></svg>
+                      </div>
+
+
                     </div>
                     <div class="flex items-center">
                         <div class="flex items-center ms-3">
@@ -62,6 +69,8 @@
                               <img class="w-8 h-8 rounded-full" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}">
                             </button>
                             @endif
+
+
                           </div>
                           <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600" id="dropdown-user">
                             <div class="px-4 py-3" role="none">
@@ -102,105 +111,9 @@
               @livewire('navigation-menu')
 
 
-              <div class="p-4 sm:ml-64">
-                 <div class="p-4 mt-14">
-                    {{-- <div class="grid grid-cols-3 gap-4 mb-4">
-                       <div class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-                          <p class="text-2xl text-gray-400 dark:text-gray-500">
-                             <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
-                             </svg>
-                          </p>
-                       </div>
-                       <div class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-                          <p class="text-2xl text-gray-400 dark:text-gray-500">
-                             <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
-                             </svg>
-                          </p>
-                       </div>
-                       <div class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-                          <p class="text-2xl text-gray-400 dark:text-gray-500">
-                             <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
-                             </svg>
-                          </p>
-                       </div>
-                    </div>
-                    <div class="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
-                       <p class="text-2xl text-gray-400 dark:text-gray-500">
-                          <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
-                          </svg>
-                       </p>
-                    </div>
-                    <div class="grid grid-cols-2 gap-4 mb-4">
-                       <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                          <p class="text-2xl text-gray-400 dark:text-gray-500">
-                             <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
-                             </svg>
-                          </p>
-                       </div>
-                       <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                          <p class="text-2xl text-gray-400 dark:text-gray-500">
-                             <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
-                             </svg>
-                          </p>
-                       </div>
-                       <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                          <p class="text-2xl text-gray-400 dark:text-gray-500">
-                             <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
-                             </svg>
-                          </p>
-                       </div>
-                       <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                          <p class="text-2xl text-gray-400 dark:text-gray-500">
-                             <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
-                             </svg>
-                          </p>
-                       </div>
-                    </div>
-                    <div class="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
-                       <p class="text-2xl text-gray-400 dark:text-gray-500">
-                          <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
-                          </svg>
-                       </p>
-                    </div>
-                    <div class="grid grid-cols-2 gap-4">
-                       <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                          <p class="text-2xl text-gray-400 dark:text-gray-500">
-                             <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
-                             </svg>
-                          </p>
-                       </div>
-                       <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                          <p class="text-2xl text-gray-400 dark:text-gray-500">
-                             <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
-                             </svg>
-                          </p>
-                       </div>
-                       <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                          <p class="text-2xl text-gray-400 dark:text-gray-500">
-                             <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
-                             </svg>
-                          </p>
-                       </div>
-                       <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                          <p class="text-2xl text-gray-400 dark:text-gray-500">
-                             <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
-                             </svg>
-                          </p>
-                       </div>
-                    </div> --}}
+              <div  id = "box" class="p-4 sm:ml-64">
+                 <div id="box2" class="p-4 mt-14">
+
 
                     {{ $slot }}
 
