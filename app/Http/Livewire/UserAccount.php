e54118c568ee0aa0dfb67908615cf4bdc8b57959
Konarch Patterns247 <konarch@patterns247.net>
2024-04-19 00:56:04 +0530

    first commit

--- app/Http/Livewire/UserAccount.php
+++ app/Http/Livewire/UserAccount.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Company;
+use App\Models\Disposition;
+use App\Models\User;
 use Livewire\Component;
 use Illuminate\Support\Facades\DB;
 use Livewire\WithPagination;
